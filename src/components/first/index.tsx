7202b71f58fae127fe3ad0c0f854e15f8110efc3
예준 <ohman0705@naver.com>
2022-04-14 11:18:00 +0900

    MyDocument error fixs

---
+++ src/components/first/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const First = () => {
+  return (
+     <>
+       <S.wraper/>
+     </>
+  );
+}
+
+export default First;
---
+++ src/components/first/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const wraper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-color: red;
+`;

19d7d4464e28208bd1a11ae9de0068ec2b5318e4
예준 <ohman0705@naver.com>
2022-04-26 12:02:13 +0900

    add local fonts

--- src/components/first/index.tsx
+++ src/components/first/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import * as S from "./style";
+import bg from '../../assets/images/bg.png';
 
 const First = () => {
-  return (
+   return (
      <>
-        <S.wrapper>hello</S.wrapper>
+        <S.wrapper>
+           <S.WhoAmI>안녕하세요<br/>나예준입니다.</S.WhoAmI>
+        </S.wrapper>
      </>
-  );
+   );
 }
 
 export default First;
--- src/components/first/style.tsx
+++ src/components/first/style.tsx
@@ -6,4 +6,14 @@ export const wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+export const WhoAmI = styled.div`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 80px;
+  line-height: 115px;
 `;

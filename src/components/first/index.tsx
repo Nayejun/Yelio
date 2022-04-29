c7b739e166eaffd05890c7fd89213679630ed55e
예준 <ohman0705@naver.com>
2022-04-29 10:48:26 +0900

    first page edits

--- src/components/first/index.tsx
+++ src/components/first/index.tsx
@@ -8,14 +8,12 @@ const First = () => {
            <S.WhoAmI>안녕하세요<br/>나예준입니다.</S.WhoAmI>
            <S.Rectangle1/>
            <S.Rectangle2/>
-           <S.Rectangle3/>
-           <S.Rectangle4/>
-           <S.Line1/>
-           <S.Line2/>
-           <S.Blur1/>
-           <S.Blur2/>
-
-
+           {/*<S.Rectangle3/>*/}
+           {/*<S.Rectangle4/>*/}
+           {/*<S.Line1/>*/}
+           {/*<S.Line2/>*/}
+           {/*<S.Blur1/>*/}
+           {/*<S.Blur2/>*/}
         </S.wrapper>
      </>
    );
--- src/components/first/style.tsx
+++ src/components/first/style.tsx
@@ -26,62 +26,58 @@ export const WhoAmI = styled.div`
 
 export const Rectangle = styled.div`
   z-index: 0;
-  position: absolute;
   background: radial-gradient(103.86% 103.86% at 77.8% 89.02%, #5871FF 0%, #7A85FF 40.5%, #F2CEF2 96%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
-  box-shadow: 0px 23px 186px rgba(255, 255, 255, 0.2);
+  box-shadow: 0 23px 186px rgba(255, 255, 255, 0.2);
   border-radius: 359.5px;
 `;
 export const Rectangle1 = styled(Rectangle)`
-  margin-left: -50vh;
-  margin-top: -30vh;
-  width: 200px;
-  height: 200px;
+  width: 165px;
+  height: 165px;
+  margin-left: -40vh;
+  margin-top: -60vh;
 `;
 export const Rectangle2 = styled(Rectangle)`
-  width: 460px;
-  height: 460px;
-  margin-left: -90vh;
-  margin-top: 45vh;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 330px;
+  height: 330px;
+  margin-top: 63.9vh;
+  -webkit-border-radius: 0 300px 0 0;
+  -moz-border-radius: 0 300px 0 0;
 `;
 export const Rectangle3 = styled(Rectangle)`
   width: 650px;
   height: 650px;
-  margin-left: 50vh;
 `;
 export const Rectangle4 = styled(Rectangle)`
   width: 460px;
   height: 460px;
-  margin-left: 110vh;
-  margin-top: 50vh;
 `;
 export const Line1 = styled.div`
-  position: absolute;
-  margin-left: 33.5vh;
   width: 1px;
   height: 1080px;
   border: 2px solid rgba(255, 255, 255, 0.3);
   transform: rotate(22.5deg);
 `;
 export const Line2 = styled(Line1)`
-  margin-left: 59.7vh;
   transform: rotate(21deg);
 `;
 export const Blur = styled.div`
-  position: absolute;
   background: linear-gradient(90deg, rgba(122, 133, 255, 0.1) 0%, rgba(122, 133, 255, 0) 119.73%);
   border: 1px solid rgba(255, 255, 255, 0.3);
   box-sizing: border-box;
   backdrop-filter: blur(10px);
 `
 export const Blur1 = styled(Blur)`
-  margin-left: -40vh;
   width: 1600px;
   height: 848px;
   clip-path: polygon(0 0, 100% 0, 78% 100%, 0% 100%);
 `;
 export const Blur2 = styled(Blur)`
-  width: 500px;
-  height: 848px;
-  margin-left: 70vh;
+  width: 110vh;
+  height: 100vh;
   clip-path: polygon(65% 0, 100% 0, 100% 100%, 0% 100%);
 `;

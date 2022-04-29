10aa9d8b4225b7969445c697f8c5dc93b6842431
예준 <ohman0705@naver.com>
2022-04-29 09:13:48 +0900

    ls

---
+++ src/components/second/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import dynamic from "next/dynamic"
+import * as S from "./style";
+
+const Second = () => {
+   return (
+     <>
+        <S.Wrapper/>
+     </>
+   );
+}
+
+export default Second;

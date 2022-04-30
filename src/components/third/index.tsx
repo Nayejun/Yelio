025d1462a9c148597b4a516039b880678c695293
예준 <ohman0705@naver.com>
2022-04-30 19:15:50 +0900

    issue

---
+++ src/components/third/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const Third = () => {
+   return (
+     <S.Positioner>
+         hello
+     </S.Positioner>
+   );
+}
+
+export default Third;
---
+++ src/components/third/style.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Positioner = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  color: white;
+`;

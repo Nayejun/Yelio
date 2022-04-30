95d0ef0374ff6a48c6a8cd191620f85246c5d59e
예준 <ohman0705@naver.com>
2022-04-30 19:29:23 +0900

    Componenets Positioning

--- src/components/third/index.tsx
+++ src/components/third/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import * as S from "./style";
+import Image from 'next/image'
+import Profile from "../../assets/images/Profile.png";
 
 const Third = () => {
    return (
      <S.Positioner>
-         hello
+        <S.ProfileWrapper>
+           <Image
+             src={Profile.src}
+             alt="Picture of the author"
+             width={400}
+             height={450}
+           />
+           <S.Information>
+              나예준
+           </S.Information>
+        </S.ProfileWrapper>
      </S.Positioner>
    );
 }
--- src/components/third/style.tsx
+++ src/components/third/style.tsx
@@ -9,3 +9,18 @@ export const Positioner = styled.div`
   background-color: black;
   color: white;
 `;
+export const ProfileWrapper = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Information = styled.p`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 50px;
+  line-height: 86px;
+  color: #FFFFFF;
+  z-index: 1;
+`;

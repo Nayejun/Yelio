1d15c88db77bf1c03bb79346766acaf87898e3f1
예준 <ohman0705@naver.com>
2022-05-01 12:47:02 +0900

    change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type {AppProps} from 'next/app'
 import {GlobalStyle} from "../styles/GlobalStyle";
+import First from "../components/first";
+import Second from "../components/second";
+import Third from "../components/third";
 
 function MyApp({Component, pageProps}: AppProps) {
    return (
      <>
         <GlobalStyle/>
+        <First/>
+        <Second/>
+        <Third/>
         <Component {...pageProps} />
      </>
    );

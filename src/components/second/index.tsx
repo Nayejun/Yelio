297b6c3c66195043541f449e687c3a399b634dc5
예준 <ohman0705@naver.com>
2022-05-07 21:17:27 +0900

    text motion

--- src/components/second/index.tsx
+++ src/components/second/index.tsx
@@ -5,12 +5,13 @@ import * as S from "./style";
 const BackgroundImg = dynamic(() => import("./BgImg"), {
    ssr: false
 })
+const Fade = require("react-reveal/Fade")
 
 const Second = () => {
    return (
      <>
         <S.Wrapper>
-              <S.WhoAreYou>Who Am I ?</S.WhoAreYou>
+           <S.WhoAreYou><Fade top>Who Am I ?</Fade></S.WhoAreYou>
            <BackgroundImg/>
         </S.Wrapper>
      </>
--- src/components/zero/index.tsx
+++ src/components/zero/index.tsx
@@ -5,11 +5,12 @@ import dynamic from "next/dynamic";
 const BackgroundImg = dynamic(() => import("./BgImg"), {
    ssr: false
 })
+const Fade = require("react-reveal/Fade")
 
 const Zero = () => {
    return (
      <S.Positioner>
-        <S.Text>I, Can</S.Text>
+        <S.Text><Fade top>I, Can</Fade></S.Text>
         <BackgroundImg/>
      </S.Positioner>
    );
